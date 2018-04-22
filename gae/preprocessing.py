d616b72eb0d4788077bab62656b8acdab549bab1
lucashu1 <lucashu@usc.edu>
2018-04-21 20:26:26 -0700

    Recalc branches after 10000 iters instead of 1000

--- gae/preprocessing.py
+++ gae/preprocessing.py
@@ -249,7 +249,7 @@ def mask_test_edges_directed(adj, test_frac=.1, val_frac=.05,
         node1, node2 = edge[0], edge[1]
 
         # Recalculate bridges every ____ iterations to relatively recent
-        if ind % 1000 == 0:
+        if ind % 10000 == 0:
             bridge_edges = set(nx.bridges(nx.to_undirected(g))) 
 
         # Don't sample bridge edges to increase likelihood of staying connected
